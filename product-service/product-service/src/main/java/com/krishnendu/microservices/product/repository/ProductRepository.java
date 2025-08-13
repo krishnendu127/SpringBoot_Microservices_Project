f420de8991ad207c371bccec8df47ffb62df3eef
krishnendu127 <your.email@example.com>
2025-08-14 01:37:23 +0530

    refactored product-service

--- product-service/product-service/src/main/java/com/krishnendu/microservices/product/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.krishnendu.microservices.product.repository;
-
-import com.krishnendu.microservices.product.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
